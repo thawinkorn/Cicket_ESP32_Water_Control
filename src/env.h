022a5e0270dcfeb6c3995810a9780d39d846f036
thawinkorn <1412.thawinkorn@gmail.com>
2024-08-27 11:59:49 +0700

    first commit

---
+++ src/env.h
@@ -0,0 +1,10 @@
+#define WIFI_SSID "Shared WiFi 4G_1013A0" // todo
+#define WIFI_PASSWORD "12341234"         // todo
+// #define WIFI_SSID "NFT_Creative_Space_2.4GHz" // todo
+// #define WIFI_PASSWORD "thank4support"         // todo
+#define DATABASE_URL                                                           \
+  "https://"                                                                   \
+  "cicket-flutter-app-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define API_KEY "AIzaSyDxLS-ATpuDjBRv9IPHKzdnnHw9Azx5F6Y" // todo
+
+#define USERNAME "max@gmail.com"
